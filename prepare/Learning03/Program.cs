42541ea4388ef06b55ee400fdbadb730afc7d39c
Marcus <juandbeta@gmail.com>
2024-05-08 22:12:54 +0200

    Learning Activity completed

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction();
+        Console.WriteLine(fraction.GetFraction());
+        Console.WriteLine(fraction.GetDecimalValue());
+        Console.WriteLine();
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.GetFraction());
+        Console.WriteLine(fraction.GetDecimalValue());
+        Console.WriteLine();
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.GetFraction());
+        Console.WriteLine(fraction3.GetDecimalValue());
+        Console.WriteLine();
+
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine(fraction4.GetFraction());
+        Console.WriteLine(fraction4.GetDecimalValue());
     }
 }
